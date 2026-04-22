a80b856e0a841bdc2ae32c8d64ec6bc86a96dd52
modelscave <yash.shingare2412@gmail.com>
2026-04-22 23:14:46 +0530

    writing changes

--- main_finetune.py
+++ main_finetune.py
@@ -20,55 +20,55 @@ def parse_arguments():
                         help='Device to use for training (default: cuda)')
     
     # Model arguments
-    parser.add_argument('--model-name', type=str, default='vit_base_patch16_224',
+    parser.add_argument('--model_name', type=str, default='vit_base_patch16_224',
                         help='Name of the vision transformer model (default: vit_base_patch16_224)')
-    parser.add_argument('--output-dim', type=int, default=256,
+    parser.add_argument('--output_dim', type=int, default=256,
                         help='Output dimension of the encoder (default: 256)')
-    parser.add_argument('--num-classes', type=int, default=2,
+    parser.add_argument('--num_classes', type=int, default=2,
                         help='Number of classification classes (default: 2)')
-    parser.add_argument('--feature-dim', type=int, default=768,
+    parser.add_argument('--feature_dim', type=int, default=768,
                         help='Feature dimension for the model (default: 768)')
-    parser.add_argument('--patch-size', type=int, default=16,
+    parser.add_argument('--patch_size', type=int, default=16,
                         help='Patch size for image processing (default: 16)')
-    parser.add_argument('--embed-dim', type=int, default=768,
+    parser.add_argument('--embed_dim', type=int, default=768,
                         help='Embedding dimension (default: 768)')
     
     # Checkpoint arguments
-    parser.add_argument('--checkpoint-path', type=str, 
+    parser.add_argument('--checkpoint_path', type=str, 
                         default='/home/chaksuai/Yash/Masked Siemese Network/checkpoints/best_model.pt',
                         help='Path to the pre-trained checkpoint')
-    parser.add_argument('--output-path', type=str,
+    parser.add_argument('--output_path', type=str,
                         default='/home/chaksuai/Yash/best_linear_probe.pt',
                         help='Path to save the trained model')
     
     # Data arguments
-    parser.add_argument('--train-csv', type=str,
+    parser.add_argument('--train_csv', type=str,
                         default='/home/chaksuai/Yash/Data/ODIR-5k/ODIR-5K/ODIR-5K/data.xlsx',
                         help='Path to training dataset CSV file')
-    parser.add_argument('--train-dir', type=str,
+    parser.add_argument('--train_dir', type=str,
                         default='/home/chaksuai/Yash/Data/ODIR-5k/ODIR-5K/ODIR-5K/Training Images',
                         help='Path to training images directory')
-    parser.add_argument('--val-csv', type=str,
+    parser.add_argument('--val_csv', type=str,
                         default='/home/chaksuai/Yash/Data/A. RFMiD_All_Classes_Dataset/2. Groundtruths/c. RFMiD_Testing_Labels.csv',
                         help='Path to validation dataset CSV file')
-    parser.add_argument('--val-dir', type=str,
+    parser.add_argument('--val_dir', type=str,
                         default='/home/chaksuai/Yash/Data/A. RFMiD_All_Classes_Dataset/1. Original Images/c. Testing Set',
                         help='Path to validation images directory')
     
     # Training arguments
-    parser.add_argument('--batch-size', type=int, default=64,
+    parser.add_argument('--batch_size', type=int, default=64,
                         help='Batch size for training (default: 64)')
-    parser.add_argument('--learning-rate', type=float, default=0.1,
+    parser.add_argument('--learning_rate', type=float, default=0.1,
                         help='Learning rate for training (default: 0.1)')
     parser.add_argument('--momentum', type=float, default=0.9,
                         help='Momentum for optimizer (default: 0.9)')
-    parser.add_argument('--total-iters', type=int, default=100000,
+    parser.add_argument('--total_iters', type=int, default=100000,
                         help='Total number of iterations for training (default: 100000)')
-    parser.add_argument('--val-freq', type=int, default=220,
+    parser.add_argument('--val_freq', type=int, default=220,
                         help='Validation frequency in iterations (default: 220)')
     
     # Image parameters
-    parser.add_argument('--image-size', type=int, default=224,
+    parser.add_argument('--image_size', type=int, default=224,
                         help='Target image size (default: 224)')
     
     # Logging and checkpoint parameters
